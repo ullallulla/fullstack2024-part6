e24158fb3a33d0731b43c85292334149a2153c0f
ullallulla <aleksi.ojansivu@gmail.com>
2024-05-07 15:16:48 +0300

    added initial version of filter reducer

--- redux-anecdotes/src/main.jsx
+++ redux-anecdotes/src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
 import reducer from './reducers/anecdoteReducer'
