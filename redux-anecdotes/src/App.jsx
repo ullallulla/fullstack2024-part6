1f3527441fbb51b73e223b7d087652d1a2d1a9a0
Aleksi Ojansivu <aleksi.ojansivu@gmail.com>
2024-05-13 18:57:00 +0300

    ex 6.14 and 6.15 added json server for anecdotes and modified app to use the db for fetching and creating anecdotes

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -2,8 +2,21 @@ import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import anecdoteService from './services/anecdotes'
+import { setAnecdote } from './reducers/anecdoteReducer'
+
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    const fetchAnecdotes = async () =>{
+      const initialAnecdotes = await anecdoteService.getAll()
+      dispatch(setAnecdote(initialAnecdotes))
+    }
+    fetchAnecdotes()
+  }, [dispatch])
   return (
     <div>
       <h2>Anecdotes</h2>
