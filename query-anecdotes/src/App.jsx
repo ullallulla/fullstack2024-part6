de691d5b9eeea9040cb3179a9c16f23d3bfef493
Aleksi Ojansivu <aleksi.ojansivu@gmail.com>
2024-05-14 17:44:50 +0300

    ex 6.20

--- query-anecdotes/src/App.jsx
+++ query-anecdotes/src/App.jsx
@@ -1,19 +1,32 @@
+import { useQuery } from '@tanstack/react-query'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { getAnecdotes, createAnecdote, updateAnecdote } from './requests'
 
 const App = () => {
 
+  
+  
   const handleVote = (anecdote) => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    refetchOnWindowFocus: false
+  })
+  console.log(JSON.parse(JSON.stringify(result)))
+
+  if ( result.isLoading ) {
+    return <div>loading data... </div>
+  }
+
+  if ( result.isError ) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+  
+  const anecdotes = result.data
 
   return (
     <div>
